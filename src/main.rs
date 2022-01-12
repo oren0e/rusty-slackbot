91877acf3047c380fd31be2888d26ace043019a4
Oren Epshtain <oepshtain@infinidat.com>
2022-01-12 23:12:44 +0200

    Use vanilla rtm slack client in main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 pub mod bot;
 pub mod error;
 pub mod playground;
-pub mod slack;
+pub mod slack_conn;
+
+use crate::bot::RustyBot;
+use slack::RtmClient;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    let token = env::var("SLACK_TOKEN").expect("SLACK_TOKEN env var was not found");
+
+    let mut bot_handler = RustyBot {};
+    let response = RtmClient::login_and_run(&token, &mut bot_handler);
+
+    println!("{:?}", response);
+    match response {
+        Ok(_) => {}
+        Err(e) => panic!("Error: {}", e),
+    }
 }
