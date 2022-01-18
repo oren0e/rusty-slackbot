10fcdbd18ff978bf33aec6f04163cff4480d2336
oren0e <countx@gmail.com>
2022-01-18 12:06:44 +0200

    Remove redundant error variants

--- src/error.rs
+++ src/error.rs
@@ -5,10 +5,6 @@ use thiserror::Error;
 pub enum RustyBotError {
     #[error(transparent)]
     InternalServerError(#[from] anyhow::Error),
-    #[error("No code provided")]
-    MissingCode,
-    #[error("Provided rust channel does not exist, please use Stable, Beta, or Nightly")]
-    InvalidRustChannel,
     #[error(
         "The command {command} is not a valid command for the bot. Use one of !code, !eval, !help (docs, book)"
     )]
